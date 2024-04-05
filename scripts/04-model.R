6b80b894b7f60b75f0862af02f565f40cdc8e9c1
Vitocr <victorchen.chen@mail.utoronto.ca>
2024-04-05 21:57:31 +0000

    add one more model and regression diagnostic content

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -12,7 +12,7 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv(file=here::here("data/analysis_data/analysis_data.csv"))
+analysis_data <- read.csv(file=here::here("data/analysis_data/analysis_data.csv"))
 
 ### Model data ####
 first_model <-
@@ -22,10 +22,20 @@ first_model <-
   )
 summary(first_model)
 
+second_model <-
+  lm(
+    formula = Fertility_rate ~ Female_employment_rate + Per_capita_GDP +
+      Female_employment_rate*Per_capita_GDP, 
+    data = analysis_data
+  )
+summary(second_model)
 #### Save model ####
 saveRDS(
   first_model,
-  file = "models/first_model.rds"
+  file = here::here("models/first_model.rds")
 )
 
-
+saveRDS(
+  second_model,
+  file = here::here("models/second_model.rds")
+)
